e90433871486ed853a5adbe53e498d82dcf64dc0
Jack <104746198+mortonjack@users.noreply.github.com>
2022-09-18 11:43:48 +0930

    visualise board from black perspective

--- drivers/example-board-setup.cpp
+++ drivers/example-board-setup.cpp
@@ -47,33 +47,34 @@ int main() {
     pawn blackDPawn = pawn('B');
 
     // Add the white pieces
-    board.addPiece(0,0,&whiteARook);
-    board.addPiece(0,1,&whiteBKnight);
-    board.addPiece(0,2,&whiteCBishop);
-    board.addPiece(0,3,&whiteQueen);
-    board.addPiece(0,4,&whiteKing);
-    board.addPiece(0,5,&whiteFBishop);
-    board.addPiece(0,6,&whiteGKnight);
-    board.addPiece(0,7,&whiteHRook);
+    board.addPiece(7,0,&whiteARook);
+    board.addPiece(7,1,&whiteBKnight);
+    board.addPiece(7,2,&whiteCBishop);
+    board.addPiece(7,3,&whiteQueen);
+    board.addPiece(7,4,&whiteKing);
+    board.addPiece(7,5,&whiteFBishop);
+    board.addPiece(7,6,&whiteGKnight);
+    board.addPiece(7,7,&whiteHRook);
 
-    board.addPiece(1,3,&whiteDPawn);
-    board.addPiece(1,4,&whiteEPawn);
+    board.addPiece(6,3,&whiteDPawn);
+    board.addPiece(6,4,&whiteEPawn);
 
     // Add the black pieces
-    board.addPiece(7,0,&blackARook);
-    board.addPiece(7,1,&blackBKnight);
-    board.addPiece(7,2,&blackCBishop);
-    board.addPiece(7,3,&blackQueen);
-    board.addPiece(7,4,&blackKing);
-    board.addPiece(7,5,&blackFBishop);
-    board.addPiece(7,6,&blackGKnight);
-    board.addPiece(7,7,&blackHRook);
+    board.addPiece(0,0,&blackARook);
+    board.addPiece(0,1,&blackBKnight);
+    board.addPiece(0,2,&blackCBishop);
+    board.addPiece(0,3,&blackQueen);
+    board.addPiece(0,4,&blackKing);
+    board.addPiece(0,5,&blackFBishop);
+    board.addPiece(0,6,&blackGKnight);
+    board.addPiece(0,7,&blackHRook);
 
-    board.addPiece(6,3,&blackDPawn);
-    board.addPiece(6,4,&blackEPawn);
+    board.addPiece(1,3,&blackDPawn);
+    board.addPiece(1,4,&blackEPawn);
     
     // Check to ensure the board was setup properly
     board.visualiseTextBoard();
+    board.visualiseTextBoard('B');
 
     return 0;
 }
--- include/gameboard.cpp
+++ include/gameboard.cpp
@@ -32,18 +32,36 @@ void gameboard::movePiece(int oldx, int oldy, int newx, int newy) {
     }
 }
 
-void gameboard::visualiseTextBoard() {
-    cout << "  A B C D E F G H" << endl;
-    for (int rank = 0; rank < 8; rank++) {
-        cout << rank << " ";
-        for (int file = 0; file < 8; file++) {
-            if (this->board[rank][file] == nullptr) {
-                cout << ". ";
-            } else {
-                cout << this->board[rank][file]->getName() << " ";
+void gameboard::visualiseTextBoard() {visualiseTextBoard('W');};
+
+void gameboard::visualiseTextBoard(char color) {
+    if (color == 'B') {
+        cout << "  H G F E D C B A" << endl;
+        for (int rank = 7; rank >= 0; rank--) {
+            cout << rank + 1 << " ";
+            for (int file = 7; file >= 0; file--) {
+                if (this->board[rank][file] == nullptr) {
+                    cout << ". ";
+                } else {
+                    cout << this->board[rank][file]->getName() << " ";
+                }
             }
+            cout << rank + 1 << endl;
         }
-        cout << rank << endl;
+        cout << "  H G F E D C B A" << endl;
+    } else {
+        cout << "  A B C D E F G H" << endl;
+        for (int rank = 0; rank < 8; rank++) {
+            cout << rank + 1 << " ";
+            for (int file = 0; file < 8; file++) {
+                if (this->board[rank][file] == nullptr) {
+                    cout << ". ";
+                } else {
+                    cout << this->board[rank][file]->getName() << " ";
+                }
+            }
+            cout << rank + 1 << endl;
+        }
+        cout << "  A B C D E F G H" << endl;
     }
-    cout << "  A B C D E F G H" << endl;
 }
