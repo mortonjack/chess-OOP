5461979eb55dda606aab309b9665588517919537
Jack <104746198+mortonjack@users.noreply.github.com>
2022-09-19 21:35:14 +0930

    Automated knight test

--- drivers/driver-test.cpp
+++ drivers/driver-test.cpp
@@ -1,6 +1,7 @@
 #include "driver.h"
 #include "bishoptest.h"
 #include "rooktest.h"
+#include "knighttest.h"
 #include <iostream>
 using namespace std;
 
@@ -8,8 +9,10 @@ int main() {
     driver tests;
     bishoptest case1;
     rooktest case2;
+    knighttest case3;
     tests.addCase(&case1);
     tests.addCase(&case2);
+    tests.addCase(&case3);
     tests.runTests();
     return 0;
 }
---
+++ drivers/knighttest.h
@@ -0,0 +1,16 @@
+#ifndef KNIGHTTEST_H
+#define KNIGHTTEST_H
+
+#include "testcase.h"
+using namespace std;
+
+class knighttest : public testcase {
+    public:
+        knighttest();
+        knighttest(int length);
+        bool movementTest(bool display); // display determines whether 
+        bool runTests(bool display); // or not to output to stdout
+        string getMessage(bool pass);
+};
+
+#endif//KNIGHTTEST_H
