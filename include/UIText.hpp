2f3420237fea1f2cc7f95a6dfd311a334b5800cb
Thomas Frew <tfrew@localhost.localdomain>
2022-10-16 09:23:45 +1030

    Improved commenting and fixed indentation for UIText.hpp

--- include/UIText.hpp
+++ include/UIText.hpp
@@ -4,49 +4,48 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace sf;
 using namespace std;
 
-// Text to use in any SFML app
 class UIText : public Drawable, public Transformable
 {
-    public:
-        Text element;
-    
     private:
-        Font _font;
+        Font _font;  // The text's font
 
     public:
-    UIText(): UIText(Vector2f(0,0), "") {}
+        Text element; // The text element storing the text
 
-    UIText(Vector2f position, string text, int fontSize = 35, Color fontColor = Color{ 0xF5f5f5FF }) {
-        // Load and text font
-        _font.loadFromFile("./assets/Pixeloid-Mono.ttf");
-        element.setFont(_font);
+        // Default constructor
+        UIText(): UIText(Vector2f(0,0), "") {}
 
-        // Set color
-        element.setFillColor(fontColor);
+        // Constructor
+        UIText(Vector2f position, string text, int fontSize = 35, Color fontColor = Color{ 0xF5f5f5FF }) {
+            // Load and text font
+            _font.loadFromFile("./assets/Pixeloid-Mono.ttf");
+            element.setFont(_font);
 
-        // Set text content
-        element.setString(text);
+            // Set color
+            element.setFillColor(fontColor);
 
-        // Set font size;
-        element.setCharacterSize(fontSize);
+            // Set text content
+            element.setString(text);
 
-        // Set text position (rounded to the nearest pixel)
-        element.setPosition((int)position.x,(int)(position.y));
-    }
-        
-    // Draws the text
-    virtual void draw(RenderTarget& target, RenderStates states) const
-    {
-        // Apply any transformation
-        states.transform *= getTransform();
+            // Set font size;
+            element.setCharacterSize(fontSize);
 
-        // Draw the text
-        target.draw(element);
-    }
+            // Set text position (rounded to the nearest pixel)
+            element.setPosition((int)position.x,(int)(position.y));
+        }
+            
+        // Draws the text
+        virtual void draw(RenderTarget& target, RenderStates states) const
+        {
+            // Apply any transformation
+            states.transform *= getTransform();
+
+            // Draw the text
+            target.draw(element);
+        }
 };
 
 #endif
