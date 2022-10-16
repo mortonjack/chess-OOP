df382be514486e379ca2dce3c2e8051b8f5a2dd9
Thomas Frew <tfrew@localhost.localdomain>
2022-10-16 11:00:01 +1030

    Commented and cleaned up Game.hpp

--- include/Game.hpp
+++ include/Game.hpp
@@ -7,120 +7,113 @@
 #include "../include/King.h"
 #include "../include/Queen.h"
 
-
 class Game{
     private:
-    // Add list of pieces, give the pieces to player classes and then add to board
-    Gameboard* _gameboard;
-    char _colorToMove;
-
-    // Convereter between colors
-    char color2OpponentColor(char color) { return color == 'W' ? 'B' : 'W'; } 
+        Gameboard* _gameboard;  // The game's board
+        char _colorToMove;      // The color of the player-to-move
 
     public:
-    // Constructor
-    Game() { }
+        // Empty constructor
+        Game() { }
 
-    // Attempt to make a move, returning true is the move is successful and false if it is unsuccessful
-    bool move(int oldFile, int oldRank, int newFile, int newRank) {
-        // If the player not trying to move a piece, return false
-        if (_gameboard->getPiece(oldFile,oldRank) == nullptr) return false;
+        // Attempt to make a move, returning true is the move is successful and false if it is unsuccessful
+        bool move(int oldFile, int oldRank, int newFile, int newRank) {
+            // If the player not trying to move a piece, return false
+            if (_gameboard->getPiece(oldFile,oldRank) == nullptr) return false;
 
-        // If the piece being moved does not belong to the player-to-move, return false
-        if (_gameboard->getPiece(oldFile,oldRank)->getColor() != _colorToMove) return false;
+            // If the piece being moved does not belong to the player-to-move, return false
+            if (_gameboard->getPiece(oldFile,oldRank)->getColor() != _colorToMove) return false;
 
-        // Attempt to move, applying all rules of the game logic
-        bool successfulMove = _gameboard->movePiece(oldFile,oldRank, newFile, newRank);
+            // Attempt to move, applying all rules of the game logic
+            bool successfulMove = _gameboard->movePiece(oldFile,oldRank, newFile, newRank);
 
-        // If the move is successful...
-        if (successfulMove) {
-            _colorToMove = color2OpponentColor(_colorToMove); // Change the player-to-move
+            // If the move is successful...
+            if (successfulMove) {
+                _colorToMove = getOppositeColorToMove(); // Change the player-to-move
+            }
+
+            // Return whether the move was successful
+            return successfulMove;
         }
 
-        // Return whether the move was successful
-        return successfulMove;
-    }
+        // Creates a new gameboard and sets it up with all of the pieces in the appropraite position
+        void setupBoard(){
+            // Create a new game board
+            _gameboard = new Gameboard();
 
-    void loadState() {
-        /* 
-        To be added.
-        */
-    }
+            // To start, it is white-to-move
+            _colorToMove = 'W';
 
-    char getGameState() {
-        // Winning conidtions
-        if (_gameboard->isInCheckmate(_colorToMove))  return 'C';
+            // Add pieces to the game board
+            Pawn* whiteAPawn = new Pawn('W');       _gameboard->addPiece(0,1,whiteAPawn);
+            Pawn* whiteBPawn = new Pawn('W');       _gameboard->addPiece(1,1,whiteBPawn);
+            Pawn* whiteCPawn = new Pawn('W');       _gameboard->addPiece(2,1,whiteCPawn);
+            Pawn* whiteDPawn = new Pawn('W');       _gameboard->addPiece(3,1,whiteDPawn);
+            Pawn* whiteEPawn = new Pawn('W');       _gameboard->addPiece(4,1,whiteEPawn);
+            Pawn* whiteFPawn = new Pawn('W');       _gameboard->addPiece(5,1,whiteFPawn);
+            Pawn* whiteGPawn = new Pawn('W');       _gameboard->addPiece(6,1,whiteGPawn);
+            Pawn* whiteHPawn = new Pawn('W');       _gameboard->addPiece(7,1,whiteHPawn);
 
-        // Drawing coniditions
-        if (_gameboard->isInStalemate(_colorToMove)) return 'S';
-        if (_gameboard->threefoldRepetition())       return '3';
-        if (_gameboard->fiftyMoveRule())             return '5';
+            Pawn* blackAPawn = new Pawn('B');       _gameboard->addPiece(0,6,blackAPawn);
+            Pawn* blackBPawn = new Pawn('B');       _gameboard->addPiece(1,6,blackBPawn);
+            Pawn* blackCPawn = new Pawn('B');       _gameboard->addPiece(2,6,blackCPawn);
+            Pawn* blackDPawn = new Pawn('B');       _gameboard->addPiece(3,6,blackDPawn);
+            Pawn* blackEPawn = new Pawn('B');       _gameboard->addPiece(4,6,blackEPawn);
+            Pawn* blackFPawn = new Pawn('B');       _gameboard->addPiece(5,6,blackFPawn);
+            Pawn* blackGPawn = new Pawn('B');       _gameboard->addPiece(6,6,blackGPawn);
+            Pawn* blackHPawn = new Pawn('B');       _gameboard->addPiece(7,6,blackHPawn);
 
-        // Otherwise, the game is not over. Return the null character
-        return '0';
-    }
+            Rook* whiteARook = new Rook('W');       _gameboard->addPiece(0,0,whiteARook);
+            Knight* whiteBKnight = new Knight('W'); _gameboard->addPiece(1,0,whiteBKnight);
+            Bishop* whiteCBishop = new Bishop('W'); _gameboard->addPiece(2,0,whiteCBishop);
+            Queen* whiteDQueen = new Queen('W');    _gameboard->addPiece(3,0,whiteDQueen);
+            King* whiteEKing = new King('W');       _gameboard->addPiece(4,0,whiteEKing);
+            Bishop* whiteFBishop = new Bishop('W'); _gameboard->addPiece(5,0,whiteFBishop);
+            Knight* whiteGKnight = new Knight('W'); _gameboard->addPiece(6,0,whiteGKnight);
+            Rook* whiteHRook = new Rook('W');       _gameboard->addPiece(7,0,whiteHRook);
 
-    char getColorToMove() {
-        return _colorToMove;
-    }
+            Rook* blackARook = new Rook('B');       _gameboard->addPiece(0,7,blackARook);
+            Knight* blackBKnight = new Knight('B'); _gameboard->addPiece(1,7,blackBKnight);
+            Bishop* blackCBishop = new Bishop('B'); _gameboard->addPiece(2,7,blackCBishop);
+            Queen* blackDQueen = new Queen('B');    _gameboard->addPiece(3,7,blackDQueen);
+            King* blackEKing = new King('B');       _gameboard->addPiece(4,7,blackEKing);
+            Bishop* blackFBishop = new Bishop('B'); _gameboard->addPiece(5,7,blackFBishop);
+            Knight* blackGKnight = new Knight('B'); _gameboard->addPiece(6,7,blackGKnight);
+            Rook* blackHRook = new Rook('B');       _gameboard->addPiece(7,7,blackHRook);
+        }
 
-    char getOppositeColorToMove() {
-        return color2OpponentColor(_colorToMove);
-    }
+        // Loads the game from its most recent state
+        void loadState() {
+            /* 
+            To be added.
+            */
+        }
 
-    // Returns this game's game board
-    void setupBoard(){
-        // Create a new game board
-        _gameboard = new Gameboard();
+        // Returns this game's board
+        Gameboard* getBoard() { return this->_gameboard; }
 
-        // To start, it is white-to-move
-        _colorToMove = 'W';
+        // Returns a code indicating whehter the game has ended, and how this has occured
+        char getGameState() {
+            // Winning conidtions
+            if (_gameboard->isInCheckmate(_colorToMove))  return 'C';
 
-        // Add pieces to the game board
-        Pawn* whiteAPawn = new Pawn('W');       _gameboard->addPiece(0,1,whiteAPawn);
-        Pawn* whiteBPawn = new Pawn('W');       _gameboard->addPiece(1,1,whiteBPawn);
-        Pawn* whiteCPawn = new Pawn('W');       _gameboard->addPiece(2,1,whiteCPawn);
-        Pawn* whiteDPawn = new Pawn('W');       _gameboard->addPiece(3,1,whiteDPawn);
-        Pawn* whiteEPawn = new Pawn('W');       _gameboard->addPiece(4,1,whiteEPawn);
-        Pawn* whiteFPawn = new Pawn('W');       _gameboard->addPiece(5,1,whiteFPawn);
-        Pawn* whiteGPawn = new Pawn('W');       _gameboard->addPiece(6,1,whiteGPawn);
-        Pawn* whiteHPawn = new Pawn('W');       _gameboard->addPiece(7,1,whiteHPawn);
+            // Drawing coniditions
+            if (_gameboard->isInStalemate(_colorToMove)) return 'S';
+            if (_gameboard->threefoldRepetition())       return '3';
+            if (_gameboard->fiftyMoveRule())             return '5';
 
-        Pawn* blackAPawn = new Pawn('B');       _gameboard->addPiece(0,6,blackAPawn);
-        Pawn* blackBPawn = new Pawn('B');       _gameboard->addPiece(1,6,blackBPawn);
-        Pawn* blackCPawn = new Pawn('B');       _gameboard->addPiece(2,6,blackCPawn);
-        Pawn* blackDPawn = new Pawn('B');       _gameboard->addPiece(3,6,blackDPawn);
-        Pawn* blackEPawn = new Pawn('B');       _gameboard->addPiece(4,6,blackEPawn);
-        Pawn* blackFPawn = new Pawn('B');       _gameboard->addPiece(5,6,blackFPawn);
-        Pawn* blackGPawn = new Pawn('B');       _gameboard->addPiece(6,6,blackGPawn);
-        Pawn* blackHPawn = new Pawn('B');       _gameboard->addPiece(7,6,blackHPawn);
+            // Otherwise, the game is not over. Return zero.
+            return '0';
+        }
 
-        Rook* whiteARook = new Rook('W');       _gameboard->addPiece(0,0,whiteARook);
-        Knight* whiteBKnight = new Knight('W'); _gameboard->addPiece(1,0,whiteBKnight);
-        Bishop* whiteCBishop = new Bishop('W'); _gameboard->addPiece(2,0,whiteCBishop);
-        Queen* whiteDQueen = new Queen('W');    _gameboard->addPiece(3,0,whiteDQueen);
-        King* whiteEKing = new King('W');       _gameboard->addPiece(4,0,whiteEKing);
-        Bishop* whiteFBishop = new Bishop('W'); _gameboard->addPiece(5,0,whiteFBishop);
-        Knight* whiteGKnight = new Knight('W'); _gameboard->addPiece(6,0,whiteGKnight);
-        Rook* whiteHRook = new Rook('W');       _gameboard->addPiece(7,0,whiteHRook);
+        // Returns the color of the player-to-move
+        char getColorToMove() { return _colorToMove; }
 
-        Rook* blackARook = new Rook('B');       _gameboard->addPiece(0,7,blackARook);
-        Knight* blackBKnight = new Knight('B'); _gameboard->addPiece(1,7,blackBKnight);
-        Bishop* blackCBishop = new Bishop('B'); _gameboard->addPiece(2,7,blackCBishop);
-        Queen* blackDQueen = new Queen('B');    _gameboard->addPiece(3,7,blackDQueen);
-        King* blackEKing = new King('B');       _gameboard->addPiece(4,7,blackEKing);
-        Bishop* blackFBishop = new Bishop('B'); _gameboard->addPiece(5,7,blackFBishop);
-        Knight* blackGKnight = new Knight('B'); _gameboard->addPiece(6,7,blackGKnight);
-        Rook* blackHRook = new Rook('B');       _gameboard->addPiece(7,7,blackHRook);
-    }
+        // Returns the color of theplayer that isn't-to-move
+        char getOppositeColorToMove() { return _colorToMove == 'W' ? 'B' : 'W'; }
 
-    Gameboard* getBoard() {
-        return this->_gameboard;
-    }
-
-    // Destructor
-    ~Game() {
-        delete _gameboard;
-    }
-
+        // Destructor
+        ~Game() {
+            delete _gameboard;
+        }
 };
