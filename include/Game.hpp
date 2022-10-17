4131343fedc4cd228ddd46da7431f769c0c3333f
Thomas Frew <tfrew@localhost.localdomain>
2022-10-18 09:44:56 +1030

    Commented game.hpp

--- include/Game.hpp
+++ include/Game.hpp
@@ -9,162 +9,162 @@
 
 class Game{
     private:
-    // Add list of pieces, give the pieces to player classes and then add to board
-    Gameboard* gameboard;
-    char colorToMove;
-
-    // Convereter between colors
-    char color2OpponentColor(char color) { return color == 'W' ? 'B' : 'W'; } 
-
-    // Detect first move
-    bool firstMove;
+        Gameboard* gameboard;   // The game's board
+        char colorToMove;       // The color of the player to move
+        bool firstMove;         // Whether it is the first move of the game
 
     public:
-    // Constructor
-    Game() { 
-        // Create a new game board
-        gameboard = new Gameboard();
-        firstMove = true;
-    }
-
-    // Attempt to make a move, returning true is the move is successful and false if it is unsuccessful
-    bool move(int oldFile, int oldRank, int newFile, int newRank) {
-        // Create a new save state on first move
-        if (firstMove) {
-            gameboard->newSave();
-            firstMove = false;
+        // Constructor
+        Game() { 
+            // Create a new game board
+            gameboard = new Gameboard();
+            firstMove = true;
         }
 
-        // If the player not trying to move a piece, return false
-        if (gameboard->getPiece(oldFile,oldRank) == nullptr) return false;
+        // Attempt to make a move, returning true is the move is successful and false if it is unsuccessful
+        bool move(int oldFile, int oldRank, int newFile, int newRank) {
+            // Create a new save state on first move
+            if (firstMove) {
+                gameboard->newSave();
+                firstMove = false;
+            }
 
-        // If the piece being moved does not belong to the player-to-move, return false
-        if (gameboard->getPiece(oldFile,oldRank)->getColor() != colorToMove) return false;
+            // If the player not trying to move a piece, return false
+            if (gameboard->getPiece(oldFile,oldRank) == nullptr) return false;
 
-        // Attempt to move, applying all rules of the game logic
-        bool successfulMove = gameboard->movePiece(oldFile,oldRank, newFile, newRank);
+            // If the piece being moved does not belong to the player-to-move, return false
+            if (gameboard->getPiece(oldFile,oldRank)->getColor() != colorToMove) return false;
 
-        // If the move is successful...
-        if (successfulMove) {
-            colorToMove = color2OpponentColor(colorToMove); // Change the player-to-move
-            gameboard->updateSave(); // Update save file
+            // Attempt to move, applying all rules of the game logic
+            bool successfulMove = gameboard->movePiece(oldFile,oldRank, newFile, newRank);
+
+            // If the move is successful...
+            if (successfulMove) {
+                colorToMove = getOppositeColorToMove(); // Change the player-to-move
+                gameboard->updateSave(); // Update save file
+            }
+
+            // Return whether the move was successful
+            return successfulMove;
         }
 
-        // Return whether the move was successful
-        return successfulMove;
-    }
+        // Saves the game's state to board.txt and moves.txt
+        void saveState() {
+            gameboard->save();
+        }
 
-    void saveState() {
-        gameboard->save();
-    }
+        // Loads the game's state from board.txt and moves.txt
+        void loadState() {
+            gameboard->load();
+            MoveNode* prevMove = gameboard->getPrevMove();
+            if (prevMove != nullptr) {
+                colorToMove = gameboard->getPiece(prevMove->getNewFile(), prevMove->getNewRank())->getColor();
+                colorToMove = getOppositeColorToMove();
+            } else colorToMove = 'W';
+            firstMove = true;
+        }
 
-    void loadState() {
-        gameboard->load();
-        MoveNode* prevMove = gameboard->getPrevMove();
-        if (prevMove != nullptr) {
-            colorToMove = gameboard->getPiece(prevMove->getNewFile(), prevMove->getNewRank())->getColor();
-            colorToMove = color2OpponentColor(colorToMove);
-        } else colorToMove = 'W';
-        firstMove = true;
-    }
+        // Return a charcter representing the game's state
+        char getGameState() {
+            // Winning conidtions
+            if (gameboard->isInCheckmate(colorToMove))  return 'C';
 
-    char getGameState() {
-        // Winning conidtions
-        if (gameboard->isInCheckmate(colorToMove))  return 'C';
+            // Drawing coniditions
+            if (gameboard->isInStalemate(colorToMove))  return 'S';
+            if (gameboard->threefoldRepetition())       return '3';
+            if (gameboard->fiftyMoveRule())             return '5';
+            if (gameboard->insufficientMaterial())      return 'I';
 
-        // Drawing coniditions
-        if (gameboard->isInStalemate(colorToMove))  return 'S';
-        if (gameboard->threefoldRepetition())       return '3';
-        if (gameboard->fiftyMoveRule())             return '5';
-        if (gameboard->insufficientMaterial())      return 'I';
+            // Otherwise, the game is not over. Return the null character
+            return '0';
+        }
 
-        // Otherwise, the game is not over. Return the null character
-        return '0';
-    }
+        // Returns the opposite color to move
+        Gameboard* getBoard() {
+            return this->gameboard;
+        }
 
-    char getColorToMove() {
-        return colorToMove;
-    }
+        // Returns the current color-to-move
+        char getColorToMove() {
+            return colorToMove;
+        }
 
-    char getOppositeColorToMove() {
-        return color2OpponentColor(colorToMove);
-    }
+        // Returns the opposite color to move
+        char getOppositeColorToMove() {
+            return colorToMove == 'W' ? 'B' : 'W';
+        }
 
-    // Returns this game's game board
-    void setupBoard(){
-        // To start, it is white-to-move
-        colorToMove = 'W';
+        // Returns this game's game board
+        void setupBoard(){
+            // To start, it is white-to-move
+            colorToMove = 'W';
 
-        // Add pieces to the game board
-        Pawn* whiteAPawn = new Pawn('W');       gameboard->addPiece(0,1,whiteAPawn);
-        Pawn* whiteBPawn = new Pawn('W');       gameboard->addPiece(1,1,whiteBPawn);
-        Pawn* whiteCPawn = new Pawn('W');       gameboard->addPiece(2,1,whiteCPawn);
-        Pawn* whiteDPawn = new Pawn('W');       gameboard->addPiece(3,1,whiteDPawn);
-        Pawn* whiteEPawn = new Pawn('W');       gameboard->addPiece(4,1,whiteEPawn);
-        Pawn* whiteFPawn = new Pawn('W');       gameboard->addPiece(5,1,whiteFPawn);
-        Pawn* whiteGPawn = new Pawn('W');       gameboard->addPiece(6,1,whiteGPawn);
-        Pawn* whiteHPawn = new Pawn('W');       gameboard->addPiece(7,1,whiteHPawn);
+            // Add pieces to the game board
+            Pawn* whiteAPawn = new Pawn('W');       gameboard->addPiece(0,1,whiteAPawn);
+            Pawn* whiteBPawn = new Pawn('W');       gameboard->addPiece(1,1,whiteBPawn);
+            Pawn* whiteCPawn = new Pawn('W');       gameboard->addPiece(2,1,whiteCPawn);
+            Pawn* whiteDPawn = new Pawn('W');       gameboard->addPiece(3,1,whiteDPawn);
+            Pawn* whiteEPawn = new Pawn('W');       gameboard->addPiece(4,1,whiteEPawn);
+            Pawn* whiteFPawn = new Pawn('W');       gameboard->addPiece(5,1,whiteFPawn);
+            Pawn* whiteGPawn = new Pawn('W');       gameboard->addPiece(6,1,whiteGPawn);
+            Pawn* whiteHPawn = new Pawn('W');       gameboard->addPiece(7,1,whiteHPawn);
 
-        Pawn* blackAPawn = new Pawn('B');       gameboard->addPiece(0,6,blackAPawn);
-        Pawn* blackBPawn = new Pawn('B');       gameboard->addPiece(1,6,blackBPawn);
-        Pawn* blackCPawn = new Pawn('B');       gameboard->addPiece(2,6,blackCPawn);
-        Pawn* blackDPawn = new Pawn('B');       gameboard->addPiece(3,6,blackDPawn);
-        Pawn* blackEPawn = new Pawn('B');       gameboard->addPiece(4,6,blackEPawn);
-        Pawn* blackFPawn = new Pawn('B');       gameboard->addPiece(5,6,blackFPawn);
-        Pawn* blackGPawn = new Pawn('B');       gameboard->addPiece(6,6,blackGPawn);
-        Pawn* blackHPawn = new Pawn('B');       gameboard->addPiece(7,6,blackHPawn);
+            Pawn* blackAPawn = new Pawn('B');       gameboard->addPiece(0,6,blackAPawn);
+            Pawn* blackBPawn = new Pawn('B');       gameboard->addPiece(1,6,blackBPawn);
+            Pawn* blackCPawn = new Pawn('B');       gameboard->addPiece(2,6,blackCPawn);
+            Pawn* blackDPawn = new Pawn('B');       gameboard->addPiece(3,6,blackDPawn);
+            Pawn* blackEPawn = new Pawn('B');       gameboard->addPiece(4,6,blackEPawn);
+            Pawn* blackFPawn = new Pawn('B');       gameboard->addPiece(5,6,blackFPawn);
+            Pawn* blackGPawn = new Pawn('B');       gameboard->addPiece(6,6,blackGPawn);
+            Pawn* blackHPawn = new Pawn('B');       gameboard->addPiece(7,6,blackHPawn);
 
-        Rook* whiteARook = new Rook('W');       gameboard->addPiece(0,0,whiteARook);
-        Knight* whiteBKnight = new Knight('W'); gameboard->addPiece(1,0,whiteBKnight);
-        Bishop* whiteCBishop = new Bishop('W'); gameboard->addPiece(2,0,whiteCBishop);
-        Queen* whiteDQueen = new Queen('W');    gameboard->addPiece(3,0,whiteDQueen);
-        King* whiteEKing = new King('W');       gameboard->addPiece(4,0,whiteEKing);
-        Bishop* whiteFBishop = new Bishop('W'); gameboard->addPiece(5,0,whiteFBishop);
-        Knight* whiteGKnight = new Knight('W'); gameboard->addPiece(6,0,whiteGKnight);
-        Rook* whiteHRook = new Rook('W');       gameboard->addPiece(7,0,whiteHRook);
+            Rook* whiteARook = new Rook('W');       gameboard->addPiece(0,0,whiteARook);
+            Knight* whiteBKnight = new Knight('W'); gameboard->addPiece(1,0,whiteBKnight);
+            Bishop* whiteCBishop = new Bishop('W'); gameboard->addPiece(2,0,whiteCBishop);
+            Queen* whiteDQueen = new Queen('W');    gameboard->addPiece(3,0,whiteDQueen);
+            King* whiteEKing = new King('W');       gameboard->addPiece(4,0,whiteEKing);
+            Bishop* whiteFBishop = new Bishop('W'); gameboard->addPiece(5,0,whiteFBishop);
+            Knight* whiteGKnight = new Knight('W'); gameboard->addPiece(6,0,whiteGKnight);
+            Rook* whiteHRook = new Rook('W');       gameboard->addPiece(7,0,whiteHRook);
 
-        Rook* blackARook = new Rook('B');       gameboard->addPiece(0,7,blackARook);
-        Knight* blackBKnight = new Knight('B'); gameboard->addPiece(1,7,blackBKnight);
-        Bishop* blackCBishop = new Bishop('B'); gameboard->addPiece(2,7,blackCBishop);
-        Queen* blackDQueen = new Queen('B');    gameboard->addPiece(3,7,blackDQueen);
-        King* blackEKing = new King('B');       gameboard->addPiece(4,7,blackEKing);
-        Bishop* blackFBishop = new Bishop('B'); gameboard->addPiece(5,7,blackFBishop);
-        Knight* blackGKnight = new Knight('B'); gameboard->addPiece(6,7,blackGKnight);
-        Rook* blackHRook = new Rook('B');       gameboard->addPiece(7,7,blackHRook);
-    }
+            Rook* blackARook = new Rook('B');       gameboard->addPiece(0,7,blackARook);
+            Knight* blackBKnight = new Knight('B'); gameboard->addPiece(1,7,blackBKnight);
+            Bishop* blackCBishop = new Bishop('B'); gameboard->addPiece(2,7,blackCBishop);
+            Queen* blackDQueen = new Queen('B');    gameboard->addPiece(3,7,blackDQueen);
+            King* blackEKing = new King('B');       gameboard->addPiece(4,7,blackEKing);
+            Bishop* blackFBishop = new Bishop('B'); gameboard->addPiece(5,7,blackFBishop);
+            Knight* blackGKnight = new Knight('B'); gameboard->addPiece(6,7,blackGKnight);
+            Rook* blackHRook = new Rook('B');       gameboard->addPiece(7,7,blackHRook);
+        }
 
-    Gameboard* getBoard() {
-        return this->gameboard;
-    }
-
-    // Destructor
-    ~Game() {
-        // Delete pieces
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (gameboard->getPiece(i, j) != nullptr) {
-                    delete gameboard->getPiece(i, j);
+        // Destroys the game, going through past moves to destroy every piece
+        ~Game() {
+            // Delete pieces
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (gameboard->getPiece(i, j) != nullptr) {
+                        delete gameboard->getPiece(i, j);
+                    }
                 }
             }
-        }
-        // Delete captured/promoted pieces
-        MoveNode* node = gameboard->getPrevMove();
-        while (node->prev() != nullptr) {
-        // Delete captured piece
-        if (node->getCapturedPiece() != nullptr) {
-            delete node->getCapturedPiece();
-        }
-        // Delete promoted piece
-        if (node->getPromotedPiece() != nullptr) {
-            delete node->getPromotedPiece();
+            // Delete captured/promoted pieces
+            MoveNode* node = gameboard->getPrevMove();
+            while (node->prev() != nullptr) {
+            // Delete captured piece
+            if (node->getCapturedPiece() != nullptr) {
+                delete node->getCapturedPiece();
+            }
+            // Delete promoted piece
+            if (node->getPromotedPiece() != nullptr) {
+                delete node->getPromotedPiece();
+            }
+
+            // Next node
+            node = node->prev();
         }
 
-        // Next node
-        node = node->prev();
-    }
-
-        // Delete gameboard
-        delete gameboard;
-    }
+            // Delete gameboard
+            delete gameboard;
+        }
 
 };
--- include/UIMoveStack.hpp
+++ include/UIMoveStack.hpp
@@ -13,153 +13,149 @@ using namespace std;
 // A stack of chess moves, to be used in the game
 class UIMoveStack : public Drawable, public Transformable
 {
-    public:
-        UIText* textComponent;
-
     private:
         int _moveCapacity;
         vector<string> _moveHistory;
         int _pastMoves = 0;
 
+        // Converts a gameboard to a string describing its most recent move (in PGN form)
+        string gameboard2MoveString(Gameboard* gameboard) {
+            // Get the rank, file and piece type of the most recent move
+            MoveNode* move = gameboard->getPrevMove();
+
+            // The move string
+            string moveString;
+
+            // Derive attibutes of the most recent move
+            int oldFile = move->getOldFile();
+            int newFile = move->getNewFile();
+            int newRank = move->getNewRank();
+            char pieceType = gameboard->getPiece(newFile, newRank)->getType();
+            pieceType = move->getPromotedPiece() != nullptr ? 'p' : pieceType;
+
+            // If a king moved > 1 tile left (we castled short...)
+            if (pieceType == 'k' && (newFile - oldFile) > 1) {
+                moveString += "O-O";                                        // Append short-castle symbol
+            // If a king moved > 1 tile right (we castled) long...
+            } else if (pieceType == 'k' && (newFile - oldFile) < -1) {
+                moveString += "O-O-O";                                      // Append long-castle symbol
+            // Otherwise...
+            } else {
+                moveString += pieceType2String(pieceType);                  // Append piece type
+                if (move->getCapturedPiece() != nullptr) moveString += 'x'; // Append capture symbol (if applicable)
+                moveString += file2String(newFile) + rank2String(newRank);  // Append target coordinates
+                if (move->getPromotedPiece() != nullptr) moveString += "=Q";                   // Append promotion (if appliable)
+            }
+
+            if (gameboard->isInMate('W') || gameboard->isInMate('B')) moveString += '#'; // Append mate symbol (if applicable)
+            else if (gameboard->isInCheck('W') || gameboard->isInCheck('B')) moveString += '+'; // Otherwise, append check symbol (if applicable)
+
+            return moveString;
+        }
+
+        // Converts from a rank's array number to its string value
+        string rank2String(int rank) { return to_string(rank + 1); }
+
+        // Converts from a file's string to its string value
+        string file2String(int file) { return string(1,file+97); }
+
+        // Converts from a piece's type to its string value
+        string pieceType2String(char type) { return (type == 'p') ? "" : string(1,toupper(type)); }
+
     public:
-    // Constructor
-    UIMoveStack(Vector2f position, int capacity, int fontSize = 35, Color fontColor = Color{ 0xF5F5F5FF }) {
-        // Create the text for the move stack
-        textComponent = new UIText(position, "", fontSize, fontColor);
+        UIText* textComponent; // The text component for the move stack
 
-        // Store the number of moves this stack can hold
-        _moveCapacity = capacity;
-    }
+        // Constructor
+        UIMoveStack(Vector2f position, int capacity, int fontSize = 35, Color fontColor = Color{ 0xF5F5F5FF }) {
+            // Create the text for the move stack
+            textComponent = new UIText(position, "", fontSize, fontColor);
 
-    // Resets & recalculates move stack
-    void updateAllMoves(Gameboard* gameboard) {
-        resetMoveStack();
-        _pastMoves = gameboard->getMoveCount();
-        bool blackMove = _pastMoves % 2 == 1;
-        _pastMoves /= 2;
-        _pastMoves -= _moveCapacity;
+            // Store the number of moves this stack can hold
+            _moveCapacity = capacity;
+        }
 
-        // Update moves 12 times
-        for (int i = blackMove ? _moveCapacity*2 : _moveCapacity*2 - 1; i > 0; i--) {
-            if (_pastMoves >= 0) {
-                gameboard->reverseBoard(i);
-                updateMovesDisplayed(gameboard);
-                gameboard->unreverseBoard(i);
-            } else if ((blackMove && i % 2 == 1) || (!blackMove && i % 2 != 1)) {
+        // Resets & recalculates move stack
+        void updateAllMoves(Gameboard* gameboard) {
+            resetMoveStack();
+            _pastMoves = gameboard->getMoveCount();
+            bool blackMove = _pastMoves % 2 == 1;
+            _pastMoves /= 2;
+            _pastMoves -= _moveCapacity;
+
+            // Update moves 12 times
+            for (int i = blackMove ? _moveCapacity*2 : _moveCapacity*2 - 1; i > 0; i--) {
+                if (_pastMoves >= 0) {
+                    gameboard->reverseBoard(i);
+                    updateMovesDisplayed(gameboard);
+                    gameboard->unreverseBoard(i);
+                } else if ((blackMove && i % 2 == 1) || (!blackMove && i % 2 != 1)) {
+                    _pastMoves++;
+                }
+            }
+            // Update previous move
+            updateMovesDisplayed(gameboard);
+        }
+    
+        // Updates the moves displayed with the most recent one
+        void updateMovesDisplayed(Gameboard* gameboard) {
+            string moveString = gameboard2MoveString(gameboard);
+
+            // If our move history is overflowing, remove the first element
+            if((int)_moveHistory.size() > _moveCapacity) {
+                _moveHistory.erase(_moveHistory.begin());
+                _moveHistory.erase(_moveHistory.begin() + 1);
                 _pastMoves++;
             }
-        }
-        // Update previous move
-        updateMovesDisplayed(gameboard);
-    }
- 
-    // Updates the moves displayed with the most recent one
-    void updateMovesDisplayed(Gameboard* gameboard) {
-        string moveString = gameboard2MoveString(gameboard);
 
-        // If our move history is overflowing, remove the first element
-        if((int)_moveHistory.size() > _moveCapacity) {
-            _moveHistory.erase(_moveHistory.begin());
-            _moveHistory.erase(_moveHistory.begin() + 1);
-            _pastMoves++;
-        }
+            // Add the most recent move to our move history
+            _moveHistory.push_back(moveString);
 
-        // Add the most recent move to our move history
-        _moveHistory.push_back(moveString);
+            // Reset text stack and move number counter
+            textComponent->getElement()->setString("");
+            int moveNumber = 1;
 
-        // Reset text stack and move number counter
-        textComponent->getElement()->setString("");
-        int moveNumber = 1;
+            // For each pair of moves
+            for (int i = 0; i < (int)_moveHistory.size(); i += 2) {
+                // Determine the move made by white and black, making the black move blank if it has not been made yet
+                string whiteMove;
+                string blackMove;
 
-        // For each pair of moves
-        for (int i = 0; i < (int)_moveHistory.size(); i += 2) {
-            // Determine the move made by white and black, making the black move blank if it has not been made yet
-            string whiteMove;
-            string blackMove;
+                if (i == (int)_moveHistory.size() - 1) {
+                    whiteMove = _moveHistory[i];
+                    blackMove = "";
+                } else {
+                    whiteMove = _moveHistory[i];
+                    blackMove = _moveHistory[i+1];
+                }
+                
+                // Create a string describing the most recent move pair
+                string moveString = to_string(moveNumber+_pastMoves) + ". " + whiteMove + " " + blackMove + "\n";
+                string newString = textComponent->getElement()->getString().toAnsiString() + moveString;
+                
+                // Update the move stack string
+                textComponent->getElement()->setString(newString);
 
-            if (i == (int)_moveHistory.size() - 1) {
-                whiteMove = _moveHistory[i];
-                blackMove = "";
-            } else {
-                whiteMove = _moveHistory[i];
-                blackMove = _moveHistory[i+1];
+                // Increment the move number being counted
+                moveNumber++;
             }
-            
-            // Create a string describing the most recent move pair
-            string moveString = to_string(moveNumber+_pastMoves) + ". " + whiteMove + " " + blackMove + "\n";
-            string newString = textComponent->getElement()->getString().toAnsiString() + moveString;
-            
-            // Update the move stack string
-            textComponent->getElement()->setString(newString);
-
-            // Increment the move number being counted
-            moveNumber++;
-        }
-    }
-
-    // Clears and resets the move stack
-    void resetMoveStack() {
-        textComponent->getElement()->setString("");
-        _moveHistory.clear();
-        _pastMoves = 0;
-    }
-
-    private:
-    // Converts a gameboard to a string describing its most recent move (in PGN form)
-    string gameboard2MoveString(Gameboard* gameboard) {
-        // Get the rank, file and piece type of the most recent move
-        MoveNode* move = gameboard->getPrevMove();
-
-        // The move string
-        string moveString;
-
-        // Derive attibutes of the most recent move
-        int oldFile = move->getOldFile();
-        int newFile = move->getNewFile();
-        int newRank = move->getNewRank();
-        char pieceType = gameboard->getPiece(newFile, newRank)->getType();
-        pieceType = move->getPromotedPiece() != nullptr ? 'p' : pieceType;
-
-        // If a king moved > 1 tile left (we castled short...)
-        if (pieceType == 'k' && (newFile - oldFile) > 1) {
-            moveString += "O-O";                                        // Append short-castle symbol
-        // If a king moved > 1 tile right (we castled) long...
-        } else if (pieceType == 'k' && (newFile - oldFile) < -1) {
-            moveString += "O-O-O";                                      // Append long-castle symbol
-        // Otherwise...
-        } else {
-            moveString += pieceType2String(pieceType);                  // Append piece type
-            if (move->getCapturedPiece() != nullptr) moveString += 'x'; // Append capture symbol (if applicable)
-            moveString += file2String(newFile) + rank2String(newRank);  // Append target coordinates
-            if (move->getPromotedPiece() != nullptr) moveString += "=Q";                   // Append promotion (if appliable)
         }
 
-        if (gameboard->isInMate('W') || gameboard->isInMate('B')) moveString += '#'; // Append mate symbol (if applicable)
-        else if (gameboard->isInCheck('W') || gameboard->isInCheck('B')) moveString += '+'; // Otherwise, append check symbol (if applicable)
+        // Clears and resets the move stack
+        void resetMoveStack() {
+            textComponent->getElement()->setString("");
+            _moveHistory.clear();
+            _pastMoves = 0;
+        }
 
-        return moveString;
-    }
+        // Draws the UI move stack
+        virtual void draw(RenderTarget& target, RenderStates states) const
+        {
+            // apply the transform
+            states.transform *= getTransform();
 
-    // Converts from a rank's array number to its string value
-    string rank2String(int rank) { return to_string(rank + 1); }
-
-    // Converts from a file's string to its string value
-    string file2String(int file) { return string(1,file+97); }
-
-    // Converts from a piece's type to its string value
-    string pieceType2String(char type) { return (type == 'p') ? "" : string(1,toupper(type)); }
-
-    public:
-
-    // Draws the UI move stack
-    virtual void draw(RenderTarget& target, RenderStates states) const
-    {
-        // apply the transform
-        states.transform *= getTransform();
-
-        // draw the vertex array
-        target.draw(*textComponent);
-    }
+            // draw the vertex array
+            target.draw(*textComponent);
+        }
 };
 
 #endif
